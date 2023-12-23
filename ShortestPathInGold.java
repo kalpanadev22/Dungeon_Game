f813f456189042404569cf0cb6a4ea1020407e92
kalpana@77 <102209171+kalpanadev22@users.noreply.github.com>
2023-12-23 22:39:15 +0530

    Update ShortestPathInGold.java

--- ShortestPathInGold.java
+++ ShortestPathInGold.java
@@ -7,73 +7,63 @@ import java.util.Scanner;
 
 public class ShortestPathInGold {
 
-	static int shortestPath(int[][] dungeon, int AdventurerRow, int AdventurerCol, int goldRow, int goldCol, int monsRow, int monsCol) {
-	    int steps = 0;
-	    int currentX = AdventurerRow;
-	    int currentY = AdventurerCol;
+	 static int shortestPath(int[][] dungeon, int adventurerRow, int adventurerCol, int goldRow, int goldCol, int monsRow, int monsCol) {
+	        int steps = 0;
+	        int currentX = adventurerRow;
+	        int currentY = adventurerCol;
 
-	    if (monsRow <= AdventurerRow && monsCol <= AdventurerCol) {
-	        return -1; 
-	    }
+	        while (currentX != goldRow || currentY != goldCol) {
+	            if (currentX < goldRow && !(currentX + 1 == monsRow && currentY == monsCol)) {
+	                currentX++;
+	            } else if (currentX > goldRow && !(currentX - 1 == monsRow && currentY == monsCol)) {
+	                currentX--;
+	            } else if (currentY < goldCol && !(currentX == monsRow && currentY + 1 == monsCol)) {
+	                currentY++;
+	            } else if (currentY > goldCol && !(currentX == monsRow && currentY - 1 == monsCol)) {
+	                currentY--;
+	            }
 
-	    while (currentX != goldRow || currentY != goldCol) {
-	        int advMoves = Math.abs(currentX - goldRow) + Math.abs(currentY - goldCol);
-	        int monsterMoves = Math.abs(monsRow - goldRow) + Math.abs(monsCol - goldCol);
-
-	        if (advMoves <= monsterMoves) {
-	            return steps + advMoves;
+	            steps++;
+	            
+	            if (steps > dungeon.length * dungeon[0].length) {
+	                return -1; 
+	            }
 	        }
 
-	        if (currentX < goldRow) {
-	            currentX++;
-	        } else if (currentX > goldRow) {
-	            currentX--;
-	        } else if (currentY < goldCol) {
-	            currentY++;
-	        } else if (currentY > goldCol) {
-                 currentY--;
-	       }
-
-	        steps++;
+	        return steps;
 	    }
 
-	    return steps;
+	    public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int[][] dungeon = {
+	            {0, 0, 0, 0, 0},
+	            {0, 0, 0, 1, 0},
+	            {0, 0, 0, 0, 0},
+	            {0, 0, 0, 0, 0},
+	            {0, 0, 0, 0, 0},
+	            {0, 0, 0, 0, 0}
+	        };
+
+	        System.out.println("Enter the AdventurerRow and AdventurerCol");
+	        int AdventurerRow = in.nextInt(); // 5;
+	        int AdventurerCol = in.nextInt(); // 1;
+
+	        System.out.println("Enter the GoldRow and GoldCol");
+	        int goldRow = in.nextInt(); // 4;
+	        int goldCol = in.nextInt(); // 3;
+
+	        System.out.println("Enter the MonsterRow and MonsterCol");
+	        int monsterRow = in.nextInt(); // 3;
+	        int monsterCol = in.nextInt(); // 1;
+
+	        int steps = shortestPath(dungeon, AdventurerRow, AdventurerCol, goldRow, goldCol, monsterRow, monsterCol);
+
+	        if (steps == -1) {
+	            System.out.println("No possible solution");
+	        } else {
+	            System.out.println("Minimum number of steps: " + steps);
+	        }
+	    }
 	}
 
 
-    public static void main(String[] args) {
-    	Scanner in=new Scanner(System.in);
-        int[][] dungeon= {
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 1, 0},
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0}
-        };
-        
-        System.out.println("Enter the AdventurerRow and AdventurerCol");
-        int AdventurerRow = in.nextInt(); //5;
-        int AdventurerCol = in.nextInt(); //1;
-        
-        System.out.println("Enter the GoldRow and GoldCol");
-        int goldRow = in.nextInt();  //1;
-        int goldCol = in.nextInt();  //4;
-        
-
-        System.out.println("Enter the MonsterRow and MonsterCol");
-        int monsterRow = in.nextInt();  //1;
-        int monsterCol = in.nextInt();  //4;
-
-        int steps = shortestPath(dungeon,  AdventurerRow, AdventurerCol, goldRow, goldCol,
-        		                    monsterRow,monsterCol);
-       if(steps==-1) {
-
-           System.out.println("No possible solution ");
-       }
-       else {
-            System.out.println("Minimum number of steps: " + steps);
-      
-    }
-    }
-}
